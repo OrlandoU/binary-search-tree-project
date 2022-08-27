edce96c8769b89f488bef91cb68639f170045fbe
Orlando Jose Umanzor Zelaya <orlandojose729@gmail.com>
2022-08-26 21:34:59 -0600

    Add script for testing

--- bst.js
+++ bst.js
@@ -1,4 +1,5 @@
-function Node(val){
+//Factory function for Nodes of BST
+function Node(val) {
     let data = val
     let left = null
     let right = null
@@ -6,13 +7,16 @@ function Node(val){
     return { data, left, right }
 }
 
-function Tree(arr){
+//Factory function for BST
+function Tree(arr) {
+    //Filters unique numbers and sorts them
     let sortArr = (arr) => {
-        return [...new Set(arr)].sort((a,b)=>a-b)
+        return [...new Set(arr)].sort((a, b) => a - b)
     }
 
+    //Build Tree, accepts sorted array as parameter
     let buildTree = (arr) => {
-        if(!arr.length) return null
+        if (!arr.length) return null
 
         let node = Node(arr[Math.trunc(arr.length / 2)])
 
@@ -21,19 +25,22 @@ function Tree(arr){
 
         return node
     }
-    let displayTree = () =>{
+
+    //Displays formatted tree
+    let displayTree = () => {
         prettyPrint(root)
     }
 
+    //Inserts Node at given index
     let insertNode = (value, node = root) => {
-        if(value > node.data) {
-            if(node.right == null) {
+        if (value > node.data) {
+            if (node.right == null) {
                 node.right = Node(value)
             }
             insertNode(value, node.right)
         }
-        if(value < node.data) {
-            if(node.left == null) {
+        if (value < node.data) {
+            if (node.left == null) {
                 node.left = Node(value)
             }
             insertNode(value, node.left)
@@ -41,110 +48,153 @@ function Tree(arr){
         array.push(value)
     }
 
+    //Deletes Node that matches given parameter
     let deleteNode = (value) => {
-        array.splice(array.findIndex(crr => crr == value ),1)
+        array.splice(array.findIndex(crr => crr == value), 1)
         _updateRoot()
     }
 
+    //Updates Root with current array values
     let _updateRoot = () => {
         array = sortArr(array)
         root = buildTree(array)
     }
 
-    let find = (value ,node = root) => {
-        if(!node) return false
-        else if(value == node.data) return node
+    //Returns Node that matches given parameter
+    let find = (value, node = root) => {
+        if (!node) return false
+        else if (value == node.data) return node
         return find(value, node.right) || find(value, node.left)
     }
 
-    let levelOrder = (cb ,queue = [root]) => {
-        if(!cb) return queue[0]
-        if(!queue.length) return
+    //Invokes CallBack on each node at Level Order Traversal
+    let levelOrder = (cb, queue = [root]) => {
+        if (!cb) return queue[0]
+        if (!queue.length) return
         let dequedNode = queue.shift()
         cb(dequedNode)
-        if(dequedNode.left != null){
+        if (dequedNode.left != null) {
             queue.push(dequedNode.left)
         }
-        if(dequedNode.right != null){
+        if (dequedNode.right != null) {
             queue.push(dequedNode.right)
         }
         levelOrder(cb, queue)
     }
 
+    //Invokes CallBack on each node in inorder Traversal
     let inorder = (cb, node = root) => {
-        if(!cb) return node
-        if(!node) return
+        if (!cb) return node
+        if (!node) return
         inorder(cb, node.left)
         cb(node)
         inorder(cb, node.right)
     }
+
+    //Invokes CallBack on each node in preorder Traversal
     let preorder = (cb, node = root) => {
-        if(!cb) return node
-        if(!node) return
+        if (!cb) return node
+        if (!node) return
         cb(node)
         inorder(cb, node.left)
         inorder(cb, node.right)
     }
+
+    //Invokes CallBack on each node in postorder Traversal
     let postorder = (cb, node = root) => {
-        if(!cb) return node
-        if(!node) return
+        if (!cb) return node
+        if (!node) return
         inorder(cb, node.left)
         inorder(cb, node.right)
         cb(node)
     }
 
+    //Returns height of given node
     let height = (node) => {
-        if(!node) return 0
+        if (!node) return 0
         return Math.max(height(node.left), height(node.right)) + 1
     }
 
+    //Returns number of edges in path from given node to the tree’s root node.
     let depth = (crrNode, node = root) => {
-        if(!node) return false
-        if(crrNode.data == node.data) return 0
+        if (!node) return false
+        if (crrNode.data == node.data) return 0
         let leftSubTree = depth(crrNode, node.left)
         let rightSubTree = depth(crrNode, node.right)
-        if(typeof leftSubTree == 'number'){
+        if (typeof leftSubTree == 'number') {
             return leftSubTree + 1
         }
-        else if(typeof rightSubTree == 'number'){
+        else if (typeof rightSubTree == 'number') {
             return rightSubTree + 1
         }
         return false
     }
 
+    //Checks if tree is balanced
     let isBalanced = () => {
-        if(height(root.right) === height(root.left)) return true
-        if( Math.abs(height(root.right) - height(root.left)) > 1) return false
+        if (height(root.right) === height(root.left)) return true
+        if (Math.abs(height(root.right) - height(root.left)) > 1) return false
     }
+
+    //Rebalances tree 
     let rebalanced = () => {
         _updateRoot()
     }
+
+    //Array with unsorted values
     let array = sortArr(arr)
+    //Full BST
     let root = buildTree(array)
-    return {buildTree, displayTree, insertNode, deleteNode, find, levelOrder, inorder, preorder, postorder, height, depth, root, isBalanced, rebalanced}
+
+    return { buildTree, displayTree, insertNode, deleteNode, find, levelOrder, inorder, preorder, postorder, height, depth, root, isBalanced, rebalanced }
 }
 
-let tree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-
-tree.insertNode(12)
-tree.insertNode(13)
-tree.insertNode(14)
-tree.insertNode(15)
-tree.insertNode(16)
-tree.insertNode(17)
-console.log(tree.depth(tree.find(14)))
-tree.displayTree()
-tree.rebalanced()
-tree.displayTree()
-
-
-
- function prettyPrint(node, prefix = '', isLeft = true){
+//Display formatted binary tree
+function prettyPrint(node, prefix = '', isLeft = true) {
     if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
     }
     console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
     if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
-  }
+}
+
+//Randomly Generates Numbers on given tree
+let add = (root) => {
+    for(let i = 0; i < 120; i++){
+        root.insertNode(Math.trunc(Math.random() * 350))
+    }
+}
+
+//CallBack function for script
+const cb = (item) => {
+    console.log(`${item.data},`)
+}
+
+//Test
+let tree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+console.log(`Balanced Tree: ${tree.isBalanced()}`)
+console.log('Level Order')
+tree.levelOrder(cb)
+console.log('Preorder')
+tree.preorder(cb)
+console.log(' Inorder')
+tree.inorder(cb)
+console.log('Postorder')
+tree.postorder(cb)
+add(tree)
+tree.displayTree()
+console.log(`Balanced Tree: ${tree.isBalanced()}`)
+tree.rebalanced()
+console.log('Level Order')
+tree.levelOrder(cb)
+console.log('Preorder')
+tree.preorder(cb)
+console.log(' Inorder')
+tree.inorder(cb)
+console.log('Postorder')
+tree.postorder(cb)
+
+
+
